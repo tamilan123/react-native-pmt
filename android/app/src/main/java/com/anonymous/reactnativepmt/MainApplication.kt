31a776538a0d08aa71fe42065b66abb8d9523899
TamilVanan <tamilvanan@appstarsapplications.in>
2025-07-12 16:02:52 +0530

    Environment fixed and build success

--- android/app/src/main/java/com/anonymous/reactnativepmt/MainApplication.kt
+++ android/app/src/main/java/com/anonymous/reactnativepmt/MainApplication.kt
@@ -1,46 +1,40 @@
 package com.anonymous.reactnativepmt
 
 import android.app.Application
-import android.content.res.Configuration
-import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
-import com.facebook.react.ReactHost
-import com.facebook.react.soloader.OpenSourceMergedSoMapping
+import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
+import com.anonymous.reactnativepmt.BuildConfig
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost = object : DefaultReactNativeHost(this) {
-    override fun getPackages(): List<ReactPackage> {
-      val packages = PackageList(this).packages
-      // Add manually linked packages here if needed
-      return packages
+    override val reactNativeHost: ReactNativeHost =
+        object : DefaultReactNativeHost(this) {
+            override fun getPackages(): List<ReactPackage> = listOf() // manually add packages if needed
+
+            override fun getJSMainModuleName(): String = "index"
+
+            override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
+
+            override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
+            override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
+        }
+
+    override val reactHost: ReactHost
+        get() = getDefaultReactHost(applicationContext, reactNativeHost)
+
+    override fun onCreate() {
+        super.onCreate()
+        SoLoader.init(this, false)
+        if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+            load()
+        }
+        ReactNativeFlipper.initializeFlipper(this, reactNativeHost.reactInstanceManager)
     }
-
-    override fun getJSMainModuleName(): String = "index"
-
-    override fun getUseDeveloperSupport(): Boolean = BuildConfig.DEBUG
-
-    override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
-    override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-  }
-
-  override val reactHost: ReactHost
-    get() = reactNativeHost
-
-  override fun onCreate() {
-    super.onCreate()
-    SoLoader.init(this, OpenSourceMergedSoMapping)
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      load()
-    }
-  }
-
-  override fun onConfigurationChanged(newConfig: Configuration) {
-    super.onConfigurationChanged(newConfig)
-  }
 }
